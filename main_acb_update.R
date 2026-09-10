c2a3f5872ce872104b52a554c30b6aabae9106db
IvoVillanueva <villanueva_i1@hotmail.com>
2026-09-10 15:12:37 +0200

    Prefijo NN_ en los scripts del pipeline por orden de ejecución

--- main_acb_update.R
+++ main_acb_update.R
@@ -1,7 +1,7 @@
 # Actualización en temporada: cada script sourceado corre su parte incremental
 # (los backfill_*() no se llaman). Orden: calendario primero, pbp_clean último.
-source("calendario.R")
-source("pbp.R")
-source("boxscore.R")
-source("stats_equipos.R")
-source("pbp_clean.R")
+source("01_calendario.R")
+source("02_pbp.R")
+source("03_boxscore.R")
+source("04_stats_equipos.R")
+source("05_pbp_clean.R")
